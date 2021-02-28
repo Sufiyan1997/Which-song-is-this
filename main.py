1c485b54d4c1ab74707b8e2b76989b41c6a13291
Sufiyan Gani <sufigani@gmail.com>
2021-03-01 00:40:35 +0530

    refactored main file

--- main.py
+++ main.py
@@ -1,25 +1,24 @@
 import pickle
 import os
-from scipy.io.wavfile import read, write
 from audio_matcher import *
 import config
-import songs
+
+def read_dict(file_path):
+    data = None
+    with open(file_path, "rb") as f:
+        data = pickle.load(f)
+
+    return data
 
 if __name__ == "__main__":
     db_dir = config.get("dbOutputDir")
     db_file = os.path.join(db_dir, "db.pickle")
     song_file = os.path.join(db_dir, "metadata.pickle")
-    db = None
-    songs_metadata = None
-
-    with open(db_file, "rb") as f:
-        db = pickle.load(f)
-
-    with open(song_file, "rb") as f:
-        songs_metadata = pickle.load(f)
-
     test_folder = config.get("testSongDir")
     test_files = os.listdir(test_folder)
+    
+    db = read_dict(db_file)
+    songs_metadata = read_dict(song_file)
 
     for tf in test_files:
         print(tf)
