a799aac6e3f9eeca5da7423160bf73ae0ff97d26
Sufiyan Gani <sufigani@gmail.com>
2021-03-02 00:47:03 +0530

    fixed mp3 to wav converting error

--- songs.py
+++ songs.py
@@ -15,7 +15,7 @@ def convert_to_wav(filepath):
             os.path.dirname(filepath), name_without_extension + ".wav"
         )
         ret = subprocess.call([path_to_ffmpeg, "-i", filepath, output_file])
-        if ret > 0:
+        if ret >= 0:
             return output_file
         else:
             return None
@@ -26,11 +26,11 @@ def convert_to_wav(filepath):
 
 
 def stereo_to_mono(song):
-    if song.shape[1] == 2:
+    if song.ndim == 1:
+        song = np.reshape(song, (song.shape[0], 1))
+    elif song.shape[1] == 2:
         song = song.astype(np.int)
         song = (song.sum(axis=1) / 2).astype(np.int16)
-    elif song.shape[1] == 0:
-        song = np.reshape(song, (song.shape[0], 1))
     return song
 
 def validate_song(song, rate):
